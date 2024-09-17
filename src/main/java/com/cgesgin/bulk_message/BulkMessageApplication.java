91352b466105309e65d0f3f29bc22fec8ca3a930
cgesgin <cevher-5@hotmail.com>
2024-09-17 16:08:41 +0300

    walking skeleton

---
+++ src/main/java/com/cgesgin/bulk_message/BulkMessageApplication.java
@@ -0,0 +1,13 @@
+package com.cgesgin.bulk_message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgesgin/bulk_message/BulkMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgesgin.bulk_message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
